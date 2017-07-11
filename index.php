c967db9a0c1935e4eb97f1c89360e916bc2f3878
Marty Bonacci <martybonacci@gmail.com>
2017-07-11 15:42:40 -0600

    align text right

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 				background-image: url("images/milton.jpg");
 				background-size: cover;
 				color: red;
+
 			}
 			h1{
 				color: red;
@@ -16,12 +17,12 @@
 		<h1 align="right">Don't touch Dylan's red stapler!</h1>
 		<br>
 		<p align="right">Well, I generally come in at least fifteen minutes late,
-		<p>ah, I use the side door - that way Lumbergh can't see me, heh -</p>
-		<p>after that I sorta space out for an hour.</p>
+		<p align="right">ah, I use the side door - that way Lumbergh can't see me, heh -</p>
+		<p align="right">after that I sorta space out for an hour.</p>
 		<p align="right">I just stare at my desk, but it looks like I'm working. </p>
-		<p>I do that for probably another hour after lunch too, </p>
-		<p>I'd say in a given week </p>
-		<p>I probably only do about fifteen minutes of real, actual, work.</p>
+		<p align="right">I do that for probably another hour after lunch too, </p>
+		<p align="right">I'd say in a given week </p>
+		<p align="right">I probably only do about fifteen minutes of real, actual, work.</p>
 
 	</body>
 </html>
