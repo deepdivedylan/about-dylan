3afad1cb3c600dca3c766b53cae50e9ea6f453a2
Marty Bonacci <martybonacci@gmail.com>
2017-07-11 15:25:07 -0600

    creared dylan's about page

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<style>
+			body {
+				background-image: url("images/milton.jpg");
+				background-size: cover;
+			}
+		</style>
+	</head>
+	<body>
+		<h1 align="right">Don't touch Dylan's red stapler!</h1>
+		<br>
+		<p align="right">Well, I generally come in at least fifteen minutes late, ah, I use the side door - that way Lumbergh can't see me, heh - after that I sorta space out for an hour.</p>
+		<p align="right">I just stare at my desk, but it looks like I'm working. I do that for probably another hour after lunch too, I'd say in a given week I probably only do about fifteen minutes of real, actual, work.</p>
+
+	</body>
+</html>
